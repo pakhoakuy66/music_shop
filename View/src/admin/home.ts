fc2daf6d3f3e33ef0ed6bfb4bebd825272edfb6e
pakhoakuy66 <phananhkhoa41004@gmail.com>
2024-09-12 22:13:22 +0700

    create pages home of admin

---
+++ View/src/admin/home.ts
@@ -0,0 +1,64 @@
+const listItems = document.querySelector<HTMLElement>(".list_item");
+const images = listItems?.getElementsByTagName("img");
+const btnL = document.querySelector(".btn_left");
+const btnR = document.querySelector(".btn_right");
+let currents = 0;
+
+if (listItems && images && images.length > 0) {
+    const handleChangeSlide = () => {
+        let width = images[0].offsetWidth;
+
+        if (currents === images.length - 1) {
+            currents = 0;
+            listItems.style.transform = `translateX(0px)`;
+        } else {
+            currents++;
+            listItems.style.transform = `translateX(-${width * currents}px)`;
+        }
+    };
+
+    let handleEvent = setInterval(handleChangeSlide, 4000);
+
+    btnR?.addEventListener("click", () => {
+        clearInterval(handleEvent);
+        handleChangeSlide(); // Gọi hàm để thực hiện thay đổi slide khi nhấn nút
+        handleEvent = setInterval(handleChangeSlide, 4000);
+    });
+
+    btnL?.addEventListener("click", () => {
+        clearInterval(handleEvent);
+        let width = images[0].offsetWidth;
+
+        if (currents === 0) {
+            currents = images.length - 1;
+        } else {
+            currents--;
+        }
+
+        listItems.style.transform = `translateX(-${width * currents}px)`;
+
+        handleEvent = setInterval(handleChangeSlide, 4000);
+    });
+} else {
+    console.error("Element with class 'list_item' or 'img' tag not found.");
+}
+
+document
+    .getElementById("search-icon")
+    ?.addEventListener("click", function (e: MouseEvent) {
+        e.preventDefault();
+        const searchInput = document.getElementById(
+            "search-input"
+        ) as HTMLInputElement | null;
+
+        if (searchInput) {
+            if (searchInput.classList.contains("hidden")) {
+                searchInput.classList.remove("hidden");
+                searchInput.classList.add("block"); // Sử dụng 'block' thay vì 'visible' nếu dùng Tailwind CSS
+                searchInput.focus();
+            } else {
+                searchInput.classList.remove("block");
+                searchInput.classList.add("hidden");
+            }
+        }
+    });
